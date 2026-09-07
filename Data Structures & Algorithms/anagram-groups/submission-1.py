e892c1b61b9ede5e21bc8c59eb3d635150c2c4f8
Sam <sambhav12dev@gmail.com>
2026-09-07 12:01:18 +0530

    Update submission-1.py

--- Data Structures & Algorithms/anagram-groups/submission-1.py
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -12,16 +12,3 @@ class Solution:
 
         return list(res.values())
 
-        # for s in strs:
-        #     count = [0] * 26
-
-        #     for c in s:
-        #         count[ord(c) - ord("a")] += 1
-        #     key = tuple(count)
-
-        #     if key in res:
-        #         res[key].append(s)
-        #     else:
-        #         res[key] = [s]
-        
-        # return list(res.values())
